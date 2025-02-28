491a773cea83ec39da8e9a9e2f85e3a713d52b7f
Brianna Argueta <briannaarguetamusic@gmail.com>
2025-02-27 22:12:46 -0800

    Im tired this is my code combined across the board... with notes

--- Homework/Homework3/data_classes.py
+++ Homework/Homework3/data_classes.py
@@ -4,7 +4,7 @@
 
 
 import json
-class person:
+class person(object):
  def __init__( self, name, age, email):
     self.name = name 
     self.age = 22
@@ -24,19 +24,41 @@ class student (person):
         super().__init_(name,age ,email)
 
   def to_dict_ ( self, name, age, email, student_id):
-     data = super().to_dict()
-     data ['student_id']= self.student_id
-     return data  
+      
   
+ #student = student ('Brianna',22,'bmargueta@cpp.edu',123456 )
+  #ilename ='data_claases.py'
+ #with open ('data_classes.py' , 'w') as f:
+ #   jspon.dump(student.to_dict() , f, indent= 4 ) 
+
+ #with open('data_classes.py'),as f:
+    #data_classes =json.load(f)
+    #print (json.dumps(data_classes, indent-2))
+
+   data = super().to_dict()
+   data ['student_id']= self.student_id
+   return data
+  
+#undo that latter
+
+ #import jsonstudent=student(name,age,email,studentid)
+
+   #ilename = 'data_classes.py'
+  #with open(filename,'w')as f:
+ 
+  #json.dump ('w')as f:
+
+
 ## this part (below) is what online was saying to add 
 def save_to_json(self, filename):
    with open (filename,'a') as f:
+
       json.dump(self.to_dict(),f,indent=4 )
  
 @staticmethod
 def display_json_content(filename):
         try:
-            with open(filename, 'r') as f:
+            with open(filename, 'a') as f:
                 data = json.load(f)
                 print(json.dumps(data, indent=4))
         except FileNotFoundError:
