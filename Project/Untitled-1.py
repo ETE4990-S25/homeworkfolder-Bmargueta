406f794053389852dd2783acfdd034820831b718
Brianna Argueta <briannaarguetamusic@gmail.com>
2025-03-07 20:10:52 -0800

    print (f'Press enter to start') input ("enter:") print (f"Welcome, or should I say good luck")

---
+++ Project/Untitled-1.py
@@ -0,0 +1,7 @@
+echo "# project-1-mafia" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ETE4990-S25/project-1-mafia.git
+git push -u origin main
